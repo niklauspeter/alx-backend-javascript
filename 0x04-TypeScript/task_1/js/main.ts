8b7e1c1e43cb5bd3f967aaf65fffc43295485edf
klaus <oriokiklaus@gmail.com>
2023-10-05 20:38:18 +0300

    create directors interface that extrends teacher and requires an attribute calle dnumber of reports

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,35 +11,3 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number,
 }
-
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
-
-interface classInterface {
-    workOnHomework(): string,
-
-    displayName(): string
-}
-
-class StudentClass implements classInterface {
-    firstName: string;
-    lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-
-    displayName(): string {
-        return this.firstName;
-    }
-
-    workOnHomework(): string {
-        return "Currently working";
-    }
-
-}
