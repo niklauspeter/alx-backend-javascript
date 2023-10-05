27a1347a9c166143c6806bc9a4d1a98a4e4f0cdc
klaus <oriokiklaus@gmail.com>
2023-10-05 21:17:29 +0300

    create majorcredits and mirorcredits interface defining a number named credits and respective functions to return the valued major and miror credits

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,48 +1,14 @@
-interface Student {
-    firstName: string,
-    lastName: string,
-    age: number,
-    location: string
+interface MajorCredits {
+    credits: number;
+    brand: string;
 }
-
-const [student1, student2] = [
-    {
-        firstName: "Gon",
-        lastName: "Freecs",
-        age: 12,
-        location: "New-York"
-    },
-    {
-        firstName: "Suzan",
-        lastName: "Storm",
-        age: 30,
-        location: "Amsterdam"
-    }
-]
-
-const studentsList: Array<Student> = [student1, student2];
-
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-const table: HTMLTableElement = document.createElement("table");
-const thead: HTMLTableSectionElement = document.createElement("thead");
-const tbody: HTMLTableSectionElement = document.createElement("tbody");
-const rowHead: HTMLTableRowElement = thead.insertRow(0);
-const firstCellHead: HTMLTableCellElement = rowHead.insertCell(0);
-const secondCellHead: HTMLTableCellElement = rowHead.insertCell(1);
-
-firstCellHead.innerHTML = "firstName";
-secondCellHead.innerHTML = "location";
-table.append(thead);
-
-
-studentsList.forEach(student => {
-    const row: HTMLTableRowElement = tbody.insertRow(0);
-    const firstCell: HTMLTableCellElement = row.insertCell(0);
-    const secondCell: HTMLTableCellElement = row.insertCell(1);
-
-    firstCell.innerHTML = student.firstName;
-    secondCell.innerHTML = student.location;
-});
-
-table.append(tbody);
-body.append(table);
+interface MinorCredits {
+    credits: number;
+    brand: string;
+}
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+    return subject1.credits + subject2.credits;
+}
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+    return subject1.credits + subject2.credits;
+}
