3d9f7cab96d7fc7fcd50443f113d19ca9d6c13da
klaus <oriokiklaus@gmail.com>
2023-10-05 20:43:04 +0300

    write class named studentclass which accepts firs tand last name arguments. its menthod workonhomework returns the string currently working and display name returns students first name. describe contructor through interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,29 @@ interface printTeacherFunction {
 }
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface classInterface {
+    workOnHomework(): string,
+
+    displayName(): string
+}
+
+class StudentClass implements classInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    displayName(): string {
+        return this.firstName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+}
